30760575aa24e81e9956c90e27c03e78fe37d21d
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-07-03 21:51:53 +0530

    divisionNumbers

---
+++ divisionNumbers.java
@@ -0,0 +1,8 @@
+public class divisionNumbers {
+    public static void main(String[] args) {
+        int x =9;
+        int y =2;
+        float z = (float)x/y;
+        System.out.println("the result ia :"+z);
+    }
+}
