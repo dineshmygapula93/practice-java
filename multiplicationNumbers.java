d50b24e0e9fecd8e88606486f361bbfe58b29553
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-07-03 21:39:52 +0530

    multiple

---
+++ multiplicationNumbers.java
@@ -0,0 +1,8 @@
+public class multiplicationNumbers {
+    public static void main(String[] args) {
+        int a =5;
+        int b =4;
+        int c = a*b;
+        System.out.println("The multiplication of the numbers is "+c);
+    }
+}
