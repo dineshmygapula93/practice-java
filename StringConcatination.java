c62a3be7cfe5127c92d5d4b381a856e930ada3a5
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-07-03 22:04:49 +0530

    String addition

---
+++ StringConcatination.java
@@ -0,0 +1,9 @@
+public class StringConcatination {
+
+    public static void main(String[] args) {
+        String firstname = "Dinesh";
+        String lastname = " mygapula";
+        String name = firstname+lastname;
+        System.out.println(name);
+    }
+}
